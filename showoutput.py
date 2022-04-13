71118bfbbed011239dbf0d9e6157e00c8ef6eb43
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2022-04-13 22:20:06 +0600

    output

---
+++ showoutput.py
@@ -0,0 +1,30 @@
+print(5)
+print("mahidul")
+print(5.00)
+print("Hello there abakldjfalsdfj;asdfka;sdfjal;sdjfflakdfajfaklsdfklajdsf")
+print("mahidul islam")
+print("mahidul " + "islam")
+# hello mahidul islam how are you
+print("hello {} how are you".format("mahi"))
+print("hello {} how {} are you".format("mahidul","islam"))
+print("hello {1} how {0} are you".format("mahidul","islam"))
+
+result = 500
+
+print("Your result: ",result)
+print("Your result: {} mahidul".format(result))
+
+
+print('''hello
+mahidul
+islam
+
+
+
+
+
+
+
+
+swe
+219''')
